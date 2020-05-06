263c46d171d652a56a9a61b84038e8dab8b50959
ServerCopMug <63811304+ServerCopMug@users.noreply.github.com>
2020-05-06 14:43:17 -0400

    Add files via upload

---
+++ client.lua
@@ -0,0 +1,18 @@
+-----------------------------------------------------------------
+-----------------------------------------------------------------
+--------------------Made by ServerCopMug#0392--------------------
+----------------Customizable-Response-Commands-------------------
+-----------------------------------------------------------------
+
+
+--------**To add more links copy and paste the lines below**--------
+--------**To change the colors of the message follow this page: https://forum.cfx.re/t/chat-formatting-colors-bold-underline/67641**--------
+
+RegisterCommand("discord", function(source, args, rawCommand) -------- replace "discord" with any other command you want **DON'T INCLUDE /**
+    TriggerEvent("chatMessage", "^*^3[Our Discord:] ^7discord.gg/dwEg4TU") ------- change message in the ""
+end)
+
+RegisterCommand("cad", function(source, args, rawCommand) ------- replace "cad" with any other command you want DON'T INCLUDE /
+    TriggerEvent("chatMessage", "^*^3[Our CAD:] ^7https://therocketcad.com/invite?code=PrimeRP") --------- cad invite link.... or anything else you want
+end)
+
